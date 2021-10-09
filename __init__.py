9fc06a0a0ca6fde86174fb2e7c3791861a204409
max-uho <maxturnheim@gmail.com>
2021-10-09 14:20:43 +0200

    should now be useable as a module. And now?

---
+++ __init__.py
@@ -0,0 +1,1 @@
+name = 'pinotate-max-uho'
---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pinotate-max-uho",
+    version="0.0.0",
+    packages=setuptools.find_packages(),
+)
